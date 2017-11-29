e312c878848d697d35d6f0f148d579bef733ab85
500alex <32263468+500alex@users.noreply.github.com>
2017-11-29 18:23:29 +0300

    Update db.php

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=agesite',
-    'username' => 'root',
+    'username' => 'user1',
     'password' => 'Cumir700',
     'charset' => 'utf8',
 ];
